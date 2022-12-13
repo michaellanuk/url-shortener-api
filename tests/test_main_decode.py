f8fc72630bbe48ac9756600db702019b5f126e89
Michael Lan <mlan@motorway.co.uk>
2022-12-13 21:25:29 +0000

    corrected tests

--- tests/test_main_decode.py
+++ tests/test_main_decode.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 
+from handlers.encode import MAX_SHORTENED_LENGTH
 from main import app
 
 client = TestClient(app)
@@ -7,14 +8,14 @@ client = TestClient(app)
 
 def test_decode_success():
     encode_response = client.post(
-        "/encode", json={"url": "http://example.com"})
+        "/", json={"url": "http://example.com"})
     short_url = encode_response.json()["short_url"]
 
-    decode_response = client.post("/decode", json={"url": short_url})
-    assert decode_response.json()["long_url"] == "http://example.com"
+    decode_response = client.get(f"/{short_url[-MAX_SHORTENED_LENGTH:]}")
+    assert decode_response.status_code == 200
 
 
 def test_decode_no_entry_found():
-    response = client.post("/decode", json={"url": "AAA1234"})
+    response = client.get("/AAA1234")
     assert response.status_code == 404
     assert "No URL found for short URL: AAA1234" in str(response.content)
